7896640e7e969dcedfe94d4950a85c3b81a4adc7
NivethigaVeerasingam <univetha166@gmail.com>
2024-09-09 17:16:05 +0530

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1 ,backgroundColor:"plum"}}>
+      <View
+      style={{width:200 ,height:200,backgroundColor:"lightblue"}}
+      ></View>
+      <View>
+  <Text>
+    THIS IS NIVETHIGA VEERASINGAM
+  </Text>
+</View>
+      
+      <View
+      style={{width:200 ,height:200,backgroundColor:"lightgreen"}}
+      ></View>
+
+      
     </View>
+
+    
   );
 }
 
